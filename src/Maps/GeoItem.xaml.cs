ff50022a6fbe5c23199d9857aea1d8cc2cd5f9de
Jeff Wilcox <jeffwilcox@gmail.com>
2011-12-11 20:43:10 -0800

    adding Maps assembly for showing a single pushpin and the current location, from 4th and mayor. pretty unclean code though.

---
+++ src/Maps/GeoItem.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Device.Location;
+using System.Windows;
+
+// pretty sure this is dead code.
+
+namespace Maps
+{
+    public class GeoItem : INotifyPropertyChanged
+    {
+        public GeoItem(bool isSelf)
+        {
+            PlaceVisibility = isSelf ? Visibility.Collapsed : Visibility.Visible;
+            SelfVisibility = isSelf ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public Visibility PlaceVisibility { get; set; }
+        public Visibility SelfVisibility { get; set; }
+
+        public string Title { get; set; }
+        public string SecondaryText { get; set; }
+        public string SmallText { get; set; }
+
+        private GeoCoordinate _location;
+        public GeoCoordinate Location 
+        { get
+        {
+            return _location;
+        }
+            set
+            {
+                _location = value;
+                OnPropertyChanged("Location");
+            }
+        }
+        // consider images, etc. ?
+
+        /// <summary>
+        /// Event raised when a property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that has changed.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
